028389122c8e7a3e716b0307596a880d5ce8691f
ramchapagai <chapagairam@gmail.com>
2021-11-11 20:31:53 -0500

    added api call

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Users from "./Users";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+   
+  <Users/>
+  
+  )
+}
+
+export default App;
---
+++ src/Users.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useState, useEffect } from 'react'    
+import { Card, CardGroup } from 'react-bootstrap'
+
+
+export default function Users() {
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    const fetchUsers = async () => {
+        const res = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await res.json()
+        console.log(data)
+        setUsers(data)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    return (
+        <div className="container-fluid">
+              <div className="text-center text-primary">User List App using React</div>
+                <div className="row"> 
+                    
+           {loading ? <h1>Loading...</h1> : 
+      
+           users.map(user => ( 
+           
+            <CardGroup className="col-lg-3 m-1" key={user.id}>
+                <Card>
+                    <Card.Body className="text-light bg-dark">
+                        <Card.Title className="text-success">{user.name}</Card.Title>
+                        <Card.Text>
+                           <p> phone: {user.phone} </p>
+                           <p> email: {user.email} </p>
+                           <p> website: {user.website} </p>
+                          
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </CardGroup>
+     
+              ))}
+                     </div>
+        </div>
+    )
+           
+}
